bc6b211bee821ccb66884c78eb2fbf916aec5713
Momin Rather <momin.rather@gmail.com>
2023-04-25 18:24:54 +0530

    Concate array to itself

--- practice.js
+++ practice.js
@@ -132,3 +132,10 @@
   fibSeries(a, b, sum, start);
   console.log(list);
 }
+
+{
+  //Q8: Conactenate Array to itself
+  input = [1, 2, 1];
+  input = input.concat(input);
+  console.log(input);
+}
