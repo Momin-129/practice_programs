e12a39fc1b089a5a5ec2bf141751e9b37ad69c19
Momin Rather <momin.rather@gmail.com>
2023-04-25 18:20:28 +0530

    Fibonnaci Recursive

--- practice.js
+++ practice.js
@@ -105,3 +105,30 @@
   }
   console.log(list);
 }
+
+{
+  //Q7: Fibannoci Series (recursion)
+
+  let a = 0,
+    b = 1,
+    start = 1;
+
+  function fibSeries(a, b, sum, start) {
+    if (start == 7) {
+      list.push(sum);
+    } else {
+      list.push(sum);
+      a = b;
+      b = sum;
+      sum = a + b;
+      fibSeries(a, b, sum, start + 1);
+    }
+  }
+
+  let sum = a + b;
+  let list = [];
+  list.push(a);
+  list.push(b);
+  fibSeries(a, b, sum, start);
+  console.log(list);
+}
