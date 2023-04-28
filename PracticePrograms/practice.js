33890f95562f07d6d61fd348dea1007b8a9d2808
Momin Rather <momin.rather@gmail.com>
2023-04-28 15:31:31 +0530

    bind() in object

--- PracticePrograms/practice.js
+++ PracticePrograms/practice.js
@@ -1,13 +1,27 @@
-let obj1 = {
-  age: 23,
-};
+// let obj1 = {
+//   age: 23,
+// };
+//
+// let obj2 = {
+//   age: 15,
+//   display: function (location, pincode) {
+//     console.log(this.age, location, pincode);
+//   },
+// };
+//
+// obj2.display.call(obj2, "Jammu", "180001");
+//
+// let x = obj2.display.bind(obj1);
+// x("Jammu", "180001");
+//
+//
 
-let obj2 = {
-  age: 15,
-  display: function (location) {
-    console.log(this.age, location);
-  },
-};
+let object = { name: "Momin", age: 23 };
 
-let x = obj2.display.bind(obj1);
-x("Jammu");
+function display() {
+  console.log(this.age);
+}
+
+let person = display.bind(object);
+
+person();
