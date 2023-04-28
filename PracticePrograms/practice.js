a7d8bdfc450a2de8773c37712913251b5ea62fef
Momin Rather <momin.rather@gmail.com>
2023-04-28 16:03:05 +0530

    Practice class

--- PracticePrograms/practice.js
+++ PracticePrograms/practice.js
@@ -16,12 +16,29 @@
 //
 //
 
-let object = { name: "Momin", age: 23 };
+// let obj1 = {
+//   age: 23,
+// };
+//
+// let obj2 = {
+//   age: 15,
+//   display: function (location, pincode) {
+//     console.log(this.age, location, pincode);
+//   },
+// };
+//
+// obj2.display.apply(obj1, ["Jammu", "180001"]);
 
-function display() {
-  console.log(this.age);
-}
+//
 
-let person = display.bind(object);
-
-person();
+// obj2.display.call(obj2, "Jammu", "180001");
+//
+// let object = { name: "Momin", age: 23 };
+//
+// function display() {
+//   console.log(this.age);
+// }
+//
+// let person = display.bind(object);
+//
+// person();
