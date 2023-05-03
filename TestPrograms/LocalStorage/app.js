cb5d78c59c25d0ff25df391a8f0f7bf62c60312a
Momin Rather <momin.rather@gmail.com>
2023-05-03 18:09:56 +0530

    change javascript code to use formData

--- TestPrograms/LocalStorage/app.js
+++ TestPrograms/LocalStorage/app.js
@@ -1,74 +1,26 @@
-let btn = document.getElementById("btn");
-let showBtn = document.getElementById("showBtn");
-btn.addEventListener("click", function () {
-  let fullname = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let contact = document.getElementById("contact").value;
-  let address = document.getElementById("address").value;
-  let gender;
-  const genderList = document.querySelectorAll("#gender");
-  for (let i = 0; i < genderList.length; i++) {
-    if (genderList[i].checked) {
-      gender = genderList[i].value;
+let record = [];
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const obj = Object.fromEntries(formData);
+
+  let Languages = formData.getAll("language");
+  obj.language = JSON.stringify(Languages);
+
+  record.push(obj);
+  let entry = JSON.stringify(record);
+  localStorage.setItem("record", entry);
+  let dataString = localStorage.getItem("record");
+  let dataArray = JSON.parse(dataString);
+
+  let details = document.getElementById("showDetails");
+  details.innerHTML = "";
+  for (eachRecord of dataArray) {
+    for (item in eachRecord) {
+      details.innerHTML += `${item.toUpperCase()} : ${eachRecord[item]}`;
     }
   }
-  gender.id = "gender";
 
-  let languages = "";
-  const languageList = document.querySelectorAll("#language");
-  for (let i = 0; i < languageList.length; i++) {
-    if (languageList[i].checked) {
-      languages = languages.concat(languageList[i].value.toString(), ",");
-    }
-  }
-  languages.id = "languages";
-
-  let identity = document.getElementById("identity").value;
-
-  localStorage.setItem("fullName", fullname);
-  localStorage.setItem("email", email);
-  localStorage.setItem("contact", contact);
-  localStorage.setItem("address", address);
-  localStorage.setItem("gender", gender);
-  localStorage.setItem("languages", languages);
-  localStorage.setItem("identity", identity);
-  showBtn.style.display = "inline";
-});
-
-showBtn.addEventListener("click", function () {
-  let fullname = localStorage.getItem("fullName");
-  let email = localStorage.getItem("email");
-  let contact = localStorage.getItem("contact");
-  let address = localStorage.getItem("address");
-  let gender = localStorage.getItem("gender");
-  let languages = localStorage.getItem("languages");
-  let identity = localStorage.getItem("identity");
-  let msg = `
-    <h4>Personal Details</h4>
-    <table style="text-align:left;">
-      <tr>
-        <th>Full Name:</th><td>${fullname}</td>
-      </tr>
-      <tr>
-        <th>Email:</th><td>${email}</td>
-      </tr>
-      <tr>
-        <th>Contact Info:</th><td>${contact}</td>
-      </tr>
-      <tr>
-        <th>Address:</th><td>${address}</td>
-      </tr>
-      <tr>
-        <th>Gender:</th><td>${gender}</td>
-      </tr>
-      <tr>
-        <th>Languages:</th><td>${languages}</td>
-      </tr>
-      <tr>
-        <th>identity Proof:</th><td>${identity}</td>
-      </tr>
-    </table>
-    `;
-  document.getElementById("showDetails").innerHTML = msg;
-  document.getElementById("showDetails").style.display = "block";
+  details.style.display = "block";
 });
