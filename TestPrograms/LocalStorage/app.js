b685769ac5f00f924783088febfa86e063474825
Momin Rather <momin.rather@gmail.com>
2023-05-04 16:59:46 +0530

    updated css and added gird display

--- TestPrograms/LocalStorage/app.js
+++ TestPrograms/LocalStorage/app.js
@@ -1,4 +1,5 @@
 let record = [];
+let dataArray;
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,16 +12,43 @@ form.addEventListener("submit", (e) => {
   record.push(obj);
   let entry = JSON.stringify(record);
   localStorage.setItem("record", entry);
-  let dataString = localStorage.getItem("record");
-  let dataArray = JSON.parse(dataString);
+  form.reset();
 
+  document.getElementById("showDetails").style.display = "none";
+  document.getElementById("showBtn").style.display = "inline";
+});
+
+document.getElementById("showBtn").addEventListener("click", () => {
   let details = document.getElementById("showDetails");
-  details.innerHTML = "";
+  let row = document.getElementById("gridRow");
+  row.innerHTML = "";
+
+  let dataString = localStorage.getItem("record");
+  dataArray = JSON.parse(dataString);
+
   for (eachRecord of dataArray) {
+    let col = document.createElement("div");
+    col.classList.add("col-md-4", "col-sm-12");
+    row.appendChild(col);
+    let individual = document.createElement("div");
+    individual.classList.add("individual");
+    col.appendChild(individual);
+    let table = document.createElement("table");
+    individual.appendChild(table);
     for (item in eachRecord) {
-      details.innerHTML += `${item.toUpperCase()} : ${eachRecord[item]}`;
+      let tr = document.createElement("tr");
+      table.appendChild(tr);
+      let th = document.createElement("th");
+      let td = document.createElement("td");
+      let key = document.createTextNode(item.toUpperCase());
+      let value = document.createTextNode(eachRecord[item]);
+      th.appendChild(key);
+      td.appendChild(value);
+      tr.appendChild(th);
+      tr.appendChild(td);
     }
   }
 
   details.style.display = "block";
+  document.getElementById("showBtn").style.display = "none";
 });
