5af08d18b768f4b618f01473196dce2be141f307
Momin Rather <momin.rather@gmail.com>
2023-05-26 10:53:49 +0530

    Accordian Updated

--- PracticePrograms/Accordian/js/app.js
+++ PracticePrograms/Accordian/js/app.js
@@ -24,6 +24,7 @@ maxRange = i;
 $("#accordion").on("click", "#openBtn", function () {
   let open = $(this).attr("aria-expanded");
   if (maxRange < 20 && open == "true") {
+    $("#accordion").append("<h4>Added New Faq</h4>");
     for (i = maxRange; i < maxRange + 3; i++) {
       let ques = records[i].ques;
       let ans = records[i].ans;
