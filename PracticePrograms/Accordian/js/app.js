210788f42ce4eef15fe83075a7f4ce35ff36c82d
Momin Rather <momin.rather@gmail.com>
2023-05-26 11:37:44 +0530

    added funtion insted of static for first five

--- PracticePrograms/Accordian/js/app.js
+++ PracticePrograms/Accordian/js/app.js
@@ -4,20 +4,9 @@ import { appendAccordian } from "./appendFunc.js";
 let maxRange = 0;
 let i;
 for (i = 0; i < 5; i++) {
-  $("#accordion").append(`
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="heading${i}">
-          <button id="openBtn" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapseTwo">
-                    ${records[i].ques}
-                  </button>
-                </h2>
-              <div id="collapse${i}" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                ${records[0].ans} 
-              </div>
-            </div>
-          </div>
-      `);
+  let ques = records[i].ques;
+  let ans = records[i].ans;
+  appendAccordian(i, ques, ans);
 }
 maxRange = i;
 
