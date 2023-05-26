5cce05f07b7f9e7763ea01e28ce82fcdcf33c849
Momin Rather <momin.rather@gmail.com>
2023-05-26 10:43:00 +0530

    changed logic of game

--- TestPrograms/TickTakToe/script/app.js
+++ TestPrograms/TickTakToe/script/app.js
@@ -1,112 +1,19 @@
-let ticTakToe = [
-  { 0: "", 1: "", 2: "" }, //0
-  { 0: "", 1: "", 2: "" }, //1
-  { 0: "", 1: "", 2: "" }, //2
-];
+import { checkWin, drawLine } from "./functions.js";
+let turn = "X";
 
-let piece = "X";
-let win = false;
-let winner = [];
-let rCount = 0,
-  cCount = 0,
-  d1Count = 0,
-  d2Count = 0;
+$("#info").html(`Player ${turn}'s turn.`);
 
-function rowWise(i, j, piece, parentId) {
-  for (let item in ticTakToe[i]) {
-    if (ticTakToe[i][item] === piece) {
-      rCount++;
-      let div = $("#" + parentId).children("#" + item);
-      winner.push(div);
-    }
-  }
-  return rCount;
-}
-
-function colWise(i, j, piece) {
-  let row = 0;
-  for (let item of ticTakToe) {
-    if (item[j] === piece) {
-      cCount++;
-      let div = $("#r" + row).children("#" + j);
-      winner.push(div);
-    }
-    row++;
-  }
-  return cCount;
-}
-
-function diag1(piece) {
-  let row = 0;
-  ticTakToe.map((a, i) => {
-    if (a[i] == piece) {
-      d1Count++;
-      let div = $("#r" + row).children("#" + i);
-      winner.push(div);
-    }
-    row++;
-  });
-  return d1Count;
-}
-
-function diag2(piece) {
-  let row = 0;
-  ticTakToe.map((a, i) => {
-    if (a[i] == piece) {
-      d2Count++;
-      let div = $("#r" + row).children("#" + i);
-      winner.push(div);
-    }
-    row++;
-  });
-  return d2Count;
-}
-
-function Win(i, j, piece, parentId) {
-  let ret = rowWise(i, j, piece, parentId);
-
-  if (ret != 3) {
-    rCount = 0;
-    winner = [];
-    let ret = colWise(i, j, piece);
-    if (ret != 3) {
-      cCount = 0;
-      winner = [];
-      let ret = diag1(piece);
-      if (ret != 3) {
-        d1Count = 0;
-        winner = [];
-        let ret = diag2(piece);
-        if (ret != 3) {
-          d2Count = 0;
-          winner = [];
-        }
-      }
-    }
-    console.log(d1Count, d2Count);
-  }
-  if (rCount == 3 || cCount == 3 || d1Count == 3 || d2Count == 3) win = true;
-  // console.log(ret, win);
-}
-
-$(".col-md-4").click((event) => {
-  let row = $(event.target).parent();
-  let col = $(event.target);
-
-  let i = parseInt($(row).attr("value"));
-  let j = parseInt($(col).attr("id"));
-  let parentId = $(row).attr("id");
-
-  if (ticTakToe[i][j] == "") {
-    if (piece == "O") piece = "X";
-    else piece = "O";
-    ticTakToe[i][j] = piece;
-    $(col).html("<span id='token'>" + ticTakToe[i][j] + "</span>");
-    Win(i, j, piece, parentId);
-    if (win) {
-      console.log("player ", piece, " wins");
-      console.log(winner);
-      winner.forEach((item) => $(item).css("border", "4px solid red"));
-    }
+$(".box").on("click", ".col-md-4", function () {
+  $(this).html(turn);
+  const [winCount, winSet] = checkWin(turn);
+  if (winCount == 3) {
+    let line = drawLine(winSet);
+    $(`.${line}`).css("display", "block");
+    $("#info").html(`Player ${turn} Won.`);
+    $(".img").css("display", "block");
+  } else {
+    if (turn == "X") turn = "0";
+    else turn = "X";
+    $("#info").html(`Player ${turn}'s turn.`);
   }
 });
---
+++ TestPrograms/TickTakToe/script/functions.js
@@ -0,0 +1,49 @@
+// 0 1 2
+// 3 4 5
+// 6 7 8
+let winArray = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+let winCount = 0;
+let winSet = [];
+
+export function checkWin(turn) {
+  for (let arr of winArray) {
+    for (let item of arr) {
+      let value = $(`#${item}`).text();
+      if (value === turn) {
+        winCount++;
+        winSet.push(item);
+      }
+    }
+    if (winCount == 3) break;
+    else {
+      winCount = 0;
+      winSet = [];
+    }
+  }
+  return [winCount, winSet];
+}
+
+export function drawLine(winSet) {
+  let line = "";
+  let winArray = winSet.toString();
+  if (winArray === [0, 1, 2].toString()) line = "top";
+  else if (winArray === [0, 3, 6].toString()) line = "left";
+  else if (winArray === [0, 4, 8].toString()) line = "leftToright";
+  else if (winArray === [1, 4, 7].toString()) line = "centerV";
+  else if (winArray === [2, 5, 8].toString()) line = "right";
+  else if (winArray === [2, 4, 6].toString()) line = "rightToleft";
+  else if (winArray === [3, 4, 5].toString()) line = "centerH";
+  else if (winArray === [6, 7, 7].toString()) line = "bottom";
+
+  return line;
+}
